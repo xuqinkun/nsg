d77e53360d964763af211fc2f9d4d71306ab8a01
xuqinkun <xu_qinkun@163.com>
2021-07-26 17:21:55 +0800

    增加双曲空间度量方式

--- include/efanna2e/distance.h
+++ include/efanna2e/distance.h
@@ -7,12 +7,14 @@
 
 #include <x86intrin.h>
 #include <iostream>
+#include <math>
 namespace efanna2e{
   enum Metric{
     L2 = 0,
     INNER_PRODUCT = 1,
     FAST_L2 = 2,
-    PQ = 3
+    PQ = 3,
+    HPB = 4
   };
     class Distance {
     public:
@@ -321,6 +323,59 @@ namespace efanna2e{
       return result;
     }
   };
+
+    /* Distance metric in hyperbolic space */
+    class DistanceHPB : public Distance {
+    public:
+      float compare(const float* a, const float* b, unsigned size) const {
+        const float a_plus, b_plus, mul, dis;
+        a_plus = sqrt(fast_multi_vec(a, a, size) + 1);
+        b_plus = sqrt(fast_multi_vec(b, b, size) + 1);
+        mul = fast_multi_vec(a, b, size) - a_plus * b_plus;
+        dis = log(mul + sqrt(mul*mul - 1));
+        return dis;
+      }
+
+     private:
+        float fast_multi_vec(const float* a, const float* b, unsigned size) const {
+          float result = 0;
+
+#ifdef __GNUC__
+
+  #define AVX_L2SQR(addr1, addr2, dest, tmp1, tmp2) \
+      tmp1 = _mm256_loadu_ps(addr1);\
+      tmp2 = _mm256_loadu_ps(addr2);\
+      tmp1 = _mm256_sub_ps(tmp1, tmp2); \
+      tmp1 = _mm256_mul_ps(tmp1, tmp1); \
+      dest = _mm256_add_ps(dest, tmp1);
+
+      __m256 sum;
+      __m256 l0, l1;
+      __m256 r0, r1;
+      unsigned D = (size + 7) & ~7U;
+      unsigned DR = D % 16;
+      unsigned DD = D - DR;
+      const float *l = a;
+      const float *r = b;
+      const float *e_l = l + DD;
+      const float *e_r = r + DD;
+      float unpack[8] __attribute__ ((aligned (32))) = {0, 0, 0, 0, 0, 0, 0, 0};
+
+      sum = _mm256_loadu_ps(unpack);
+      if(DR){AVX_L2SQR(e_l, e_r, sum, l0, r0);}
+
+      for (unsigned i = 0; i < DD; i += 16, l += 16, r += 16) {
+      	AVX_L2SQR(l, r, sum, l0, r0);
+      	AVX_L2SQR(l + 8, r + 8, sum, l1, r1);
+      }
+      _mm256_storeu_ps(unpack, sum);
+      result = unpack[0] + unpack[1] + unpack[2] + unpack[3] + unpack[4] + unpack[5] + unpack[6] + unpack[7];
+      x_plus = sqrt(result + 1);
+#endif
+
+            return result;
+        }
+    };
 }
 
 
