87b658f86bff7990f74f22979dd98fc0c2fd7860
xuqinkun <xu_qinkun@163.com>
2021-07-29 09:27:12 +0800

    Initialize distance_

--- src/index.cpp
+++ src/index.cpp
@@ -10,6 +10,8 @@ Index::Index(const size_t dimension, const size_t n, Metric metric = L2)
     switch (metric) {
       case L2:distance_ = new DistanceL2();
         break;
+      case HPB:distance_ = new DistanceHPB();
+        break;
       default:distance_ = new DistanceL2();
         break;
     }
